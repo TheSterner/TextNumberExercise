ebd16ee6cd6db80aa928008b95c2a1f97e1040c9
William B McAllister <blakemcallister77@gmail.com>
2018-10-28 17:03:06 -0400

    pg 154

--- TextNumberExercise/Program.cs
+++ TextNumberExercise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TextNumberExercise
 {
@@ -10,6 +11,13 @@ namespace TextNumberExercise
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Please pick a number between 1-50.");
+			string userNumber = Console.ReadLine();
+			File.WriteAllText(@"C:\Users\Blake\Logs\TextNumber.txt", userNumber);
+			Console.WriteLine("Your number is: " + File.ReadAllText(@"C:\Users\Blake\Logs\TextNumber.txt"));
+			Console.ReadLine();
+
+
 		}
 	}
 }
